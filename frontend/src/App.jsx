047b6b797a5f7146ee57676f3ae6fbe62d80a5cf
Oleh <berolegnik@gmail.com>
2025-04-05 15:17:57 +0300

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Home from './pages/Home'
-import Navbar from './components/Navbar' // путь к компоненту Navbar
+import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <header className="header">
+        <Navbar />
+      </header>
+      <main className="main-content">
+        <Home />
+      </main>
     </>
   )
 }
