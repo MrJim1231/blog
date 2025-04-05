d2856b04bef234eef346511e3bb8a3bdbee12666
Oleh <berolegnik@gmail.com>
2025-04-05 18:11:16 +0300

    content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-// src/App.jsx
-import { Route, Routes } from 'react-router-dom' // Импортируем компоненты для маршрутизации
-import Home from './pages/Home' // Страница главная
-import CategoryPage from './pages/CategoryPage' // Страница категории
-import Header from './components/Header' // Импортируем Header
-import Categories from './components/Categories' // Импортируем компонент Categories
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import CategoryPage from './pages/CategoryPage'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import './App.css' // если ты используешь внешние стили, добавь этот импорт
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category" element={<Categories />} /> {/* Путь для отображения всех категорий */}
-        <Route path="/category/:category" element={<CategoryPage />} /> {/* Страница для конкретной категории */}
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Categories />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+        </Routes>
+      </div>
     </>
   )
 }
